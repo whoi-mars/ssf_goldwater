476ec6e1d2f1c0665e21417da463f8fa971ee100
Mark Goldwater <mgoldwater@olin.edu>
2020-07-20 10:16:04 -0400

    Currently applies the model to a single file in all channels

--- applyModel.py
+++ applyModel.py
@@ -9,7 +9,6 @@ from utils.scanData import scan_audiofile
 import utils.spect as spect
 import numpy as np
 import pandas as pd
-import pickle
 import os
 
 ##
@@ -17,6 +16,19 @@ import os
 """ Define helpful functions """
 
 
+def plot_grid(X):
+
+    num_per_row = 6
+    rows = np.ceil(len(X) / num_per_row)
+
+    plt.figure(figsize=(5, 5))
+    for i, image in enumerate(X):
+        plt.subplot(rows, num_per_row, i + 1)
+        plt.axis("off")
+        plt.imshow(image)
+
+    plt.subplots_adjust(hspace=0.1, wspace=0.1)
+    plt.show()
 
 
 ##
@@ -24,11 +36,15 @@ import os
 """ Set up file paths and load """
 
 # Store path to file
-file = "20150824-210000_multichannel_wav_SAMBAYu.wav"
+file = "20150827-060000_multichannel_wav_SAMBAYu.wav"
 DATA_PATH = os.path.join("C:/Users/mgoldwater/Desktop/WHOI Storage/SAMBAY",
                          "acoustic data",
-                         "multichannel_wav_1h",
-                         file)
+                         "multichannel_wav_1h")
+FILE_PATH = os.path.join(DATA_PATH, file)
+
+# Get list of audio file names
+audio_files = os.listdir(DATA_PATH)
+print(audio_files)
 
 # Directory where results should be stored
 WRITE_PATH = "C:/Users/mgoldwater/Desktop/WHOI Storage/scan_results"
@@ -37,15 +53,11 @@ WRITE_PATH = "C:/Users/mgoldwater/Desktop/WHOI Storage/scan_results"
 
 """ Load model and training data mean and process data """
 
-# Load model
-f = open("C:/Users/mgoldwater/ssf_goldwater/models/mu_1595010328", "rb")
-mu = pickle.load(f)
-f.close()
-
-model = tf.keras.models.load_model("C:/Users/mgoldwater/ssf_goldwater/models/1595010328.h5",
+model = tf.keras.models.load_model("C:/Users/mgoldwater/ssf_goldwater/models/1595113438.h5",
                                    custom_objects={'KerasLayer': hub.KerasLayer})
 
-scan_audiofile(DATA_PATH, WRITE_PATH, 2, model, mu, "calls", step_size=1000)
+for channel in range(1, 7):
+    scan_audiofile(DATA_PATH, WRITE_PATH, channel, model, "geq2_modes_calls", batch_size=500, step_size=2536)
 
 ##
 
@@ -58,13 +70,17 @@ Fs_original, samples_norm = spect.get_and_normalize_sound(DATA_PATH)
 
 """ Choose a row and calculate spectrogram"""
 
-row = 2
+X = []
 
-samples = samples_norm[calls_df.loc[row].StartSample:calls_df.loc[row].EndSample, calls_df.loc[row].Channel - 1]
-_, _, Zxx, _ = spect.my_stft(samples, Fs_original, window_N=31, window_overlap=5, NFFT=2 ** 8)
-Zxx = Zxx[round(Zxx.shape[0] / 2):, :]
-spectro = 10 * np.log10(np.abs(Zxx) ** 2)
+for row in range(len(calls_df)):
+    samples = samples_norm[calls_df.loc[row].StartSample:calls_df.loc[row].EndSample, calls_df.loc[row].Channel - 1]
+    _, _, Zxx, _ = spect.my_stft(samples, Fs_original, window_N=31, window_overlap=5, NFFT=2 ** 8)
+    Zxx = Zxx[round(Zxx.shape[0] / 2):, :]
+    spectro = 10 * np.log10(np.abs(Zxx) ** 2)
+    X.append(spectro)
 
-plt.imshow(spectro)
+##
+
+plot_grid(X)
 
 ##
